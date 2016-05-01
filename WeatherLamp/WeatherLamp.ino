73be2cf0bd6fa0157cf406a7ad2ca0d834bbd76d
NaoyaSawaguchi(sr-pc) <pmsouth.2dx@gmail.com>
2016-05-01 14:37:19 +0900

    エラーを可視化する機能を追加。 WiFiのエラー : LED_NETが消灯し、LED_SUNが点滅 設定情報のエラー（設定情報サーバに接続できない) : LED_CULが点滅 APIに接続できない : LED_RAYが点滅

--- WeatherLamp/WeatherLamp.ino
+++ WeatherLamp/WeatherLamp.ino
@@ -1,8 +1,12 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 
+// system
 #define WL_DEBUG
+#define SERIAL_SPEED 115200
 #define BUFFER_SIZE 512
+#define INTERVAL    100   // ms
+#define TIMEOUT     5000  // ms
 
 // network
 #define SSID "***"
@@ -10,9 +14,8 @@
 #define CONF_HOST "***"
 #define CONF_PATH "/ESP8266/config.txt"
 #define HTTP_PORT 80
-#define SERIAL_SPEED 115200
 
-// weather leds
+// leds
 #define LED_SUN 12
 #define LED_CUL 13
 #define LED_RAY 15
@@ -22,9 +25,10 @@
 #define RAY 0x04
 #define NET 0x08
 
-// led status
-#define ON HIGH
-#define OFF LOW
+// error codes
+#define ERR_WIFI_TIMEOUT 0x01
+#define ERR_CONF         0x02
+#define ERR_API          0x04
 
 String g_appid = "";
 String g_api_host = "";
@@ -32,6 +36,8 @@ String g_api_path = "";
 String g_api_city_id = "";
 String g_api_cnt = "";
 char   g_gp_buff[BUFFER_SIZE] = {0};
+byte   g_led_stat = 0;
+byte   g_error_code = 0;
 
 byte led_patterns[] ={
   0,        // 00 not use
@@ -67,9 +73,9 @@ void led_write(byte leds){
 //アウトプットの設定
 void setup()
 {
-  byte led_stat = 0;
   String weather_icon = "";
-  int weather_id = 0;
+  int weather_id;
+  unsigned long timeout;
 
   pinMode(LED_SUN, OUTPUT);
   pinMode(LED_CUL, OUTPUT);
@@ -80,14 +86,24 @@ void setup()
   Serial.begin(SERIAL_SPEED);
 
   // Wifi Setting
+  timeout = millis();
   WiFi.begin(SSID,PASS);
   while(WiFi.status() != WL_CONNECTED){
-    delay(100);
-    led_stat = led_stat ^ NET;
-    led_write(led_stat);
+    if(millis() - timeout > TIMEOUT){
+      g_error_code |= ERR_WIFI_TIMEOUT;
+      
+#ifdef WL_DEBUG
+      Serial.println("WiFi TIMEOUT");
+#endif // WL_DEBUG
+
+      return;
+    }
+    g_led_stat = g_led_stat ^ NET;
+    led_write(g_led_stat);
+    delay(INTERVAL);
   }
-  led_stat |= NET;
-  led_write(led_stat);
+  g_led_stat |= NET;
+  led_write(g_led_stat);
   
 #ifdef WL_DEBUG
   Serial.println("Wifi connected.");
@@ -98,26 +114,32 @@ void setup()
   // get config file
   WiFiClient client;
   if (!client.connect(CONF_HOST,HTTP_PORT)){
-    
 #ifdef WL_DEBUG
     Serial.println(String("connection failed : ") + CONF_HOST);
 #endif // WL_DEBUG
-
-    led_write(0);
+    g_error_code |= ERR_CONF;
     return;
   }
   client.print(buildUri(CONF_HOST,CONF_PATH));
-  delay(10);
+
+  timeout = millis();
+  while(client.available() == 0){
+    if(millis() - timeout > timeout){
+      client.stop();
+      g_error_code |= ERR_CONF;
+      return;
+    }
+  }
 
   while(client.available()){
-    String line = client.readStringUntil('\r');
+    String line = client.readStringUntil('\n');
     int    eq   = 0;
     line.trim();
     eq = line.indexOf('=');
     if(eq > 0){
       String key  = line.substring(0,eq);
       String val  = line.substring(eq+1);
-      
+      Serial.println("KEY:: "+key);
       if(key.equals("HOST")){
         g_api_host = val;
       }else if(key.equals("PATH")){
@@ -128,9 +150,9 @@ void setup()
         g_api_cnt = val;
       }else if(key.equals("id")){
         g_api_city_id = val;
-      }// end if
-    }//end if
-  }// end while
+      }
+    }
+  }
   
 #ifdef WL_DEBUG
   Serial.println("host  : " + g_api_host);
@@ -142,25 +164,24 @@ void setup()
 
   //get weather data
   if( (g_api_host.length() < 5) || (g_api_path.length() < 5)){
-    
+    g_error_code |= ERR_CONF;
 #ifdef WL_DEBUG
     Serial.println("ERR g_api_host :: " + g_api_host);
     Serial.println("ERR g_api_path :: " + g_api_path);
 #endif // WL_DEBUG
 
     return;
-  }//end if
+  }
 
   g_api_host.toCharArray(g_gp_buff,BUFFER_SIZE);
   if (!client.connect(g_gp_buff,HTTP_PORT)){
-    
+    g_error_code |= ERR_API;
 #ifdef WL_DEBUG
     Serial.println(String("connection failed : ") + g_api_host);
 #endif // WL_DEBUG
 
-    led_write(0);
     return;
-  }//end if
+  }
 
   g_api_path  = g_api_path + "?" +
                "APPID=" + g_appid +
@@ -172,7 +193,14 @@ void setup()
 #endif // WL_DEBUG
 
   client.print(buildUri(g_api_host,g_api_path));
-  delay(10);
+  timeout = millis();
+  while(client.available() == 0){
+    if(millis() - timeout > timeout){
+      client.stop();
+      g_error_code |= ERR_API;
+      return;
+    }
+  }
   while(client.available()){
     String line = client.readStringUntil('\r');
     int idx;
@@ -186,19 +214,40 @@ void setup()
     idx = line.lastIndexOf("icon");
     if(idx >= 0){
       weather_icon = line.substring(idx+7,idx+9);
-    }// end if
-  }// end while
+    }
+  }
 
-  led_write(0);
   weather_icon.toCharArray(g_gp_buff,BUFFER_SIZE);
   weather_id = atoi(g_gp_buff);
-  if(weather_id < sizeof(led_patterns)){
-    led_stat |= led_patterns[weather_id];
-    led_write(led_stat);
-  }// end if
+  if((0 <= weather_id) && (weather_id < sizeof(led_patterns))){
+    if(led_patterns[weather_id]){
+      g_led_stat |= led_patterns[weather_id];
+    }else{
+      g_led_stat |= SUN|CUL|RAY;
+    }
+  }else{
+    g_led_stat |= SUN|CUL|RAY;
+  }
+  led_write(g_led_stat);
 }
 
+// 主にエラー処理。エラー内容に応じてLEDを点滅させる。
 void loop(){
+  if(g_error_code & ERR_WIFI_TIMEOUT){
+    g_led_stat &= ~NET;
+    g_led_stat ^= SUN;
+  }
+
+  if(g_error_code & ERR_CONF){
+    g_led_stat ^= CUL;
+  }
+
+  if(g_error_code & ERR_API){
+    g_led_stat ^= RAY;
+  }
+  
+  led_write(g_led_stat);
+  delay(INTERVAL);
 }
 
 
